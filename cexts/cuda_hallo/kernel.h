545ca121830713efaef267a577ab7f9c7ecdbc78
supplient <437566830@qq.com>
2022-09-11 20:03:02 +0800

    cuda_hallo module

---
+++ cexts/cuda_hallo/kernel.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+#ifdef CUDA_HALLO_EXPORTS
+	#define CUDA_HALLO_API __declspec(dllexport)
+#else
+	#define CUDA_HALLO_API
+#endif
+
+void CudaTestFunc();
--- main.py
+++ main.py
@@ -3,3 +3,6 @@ sys.path.insert(0, "./cexts/dll")
 
 import hallo
 hallo.test()
+
+import cuda_hallo
+cuda_hallo.test()
